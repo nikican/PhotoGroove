a53a34598c72fbbe7598caa63eb211590a30ea05
Nikica <nikica.novakovic@gmail.com>
2017-07-31 16:12:39 +0200

    Add filter sliders

--- PhotoGroove.elm
+++ PhotoGroove.elm
@@ -1,7 +1,7 @@
 module PhotoGroove exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (id, class, classList, src, name, type_, title, checked)
+import Html.Attributes as Attr exposing (id, class, classList, src, name, type_, title, checked, max)
 import Json.Decode exposing (string, int, list, Decoder)
 import Json.Decode.Pipeline exposing (decode, required, optional)
 import Html.Events exposing (onClick)
@@ -81,12 +81,26 @@ view model =
         , button
             [ onClick SurpriseMe ]
             [ text "Surprise Me!" ]
+        , div [ class "filters" ]
+            [ viewFilter "Hue" 0
+            , viewFilter "Ripple" 0
+            , viewFilter "Noise" 0
+            ]
         , h3 [] [ text "Thumbnail Size:" ]
         , div [ id "choose-size" ] <|
             List.map (viewSizeChooser model.chosenSize) [ Small, Medium, Large ]
         , div [ id "thumbnails", class <| sizeToClass model.chosenSize ] <|
             List.map (viewThumbnail model.selectedUrl) model.photos
         , viewLarge model.selectedUrl
+        ]
+
+
+viewFilter : String -> a -> Html msg
+viewFilter name magnitude =
+    div [ class "filter-slider" ]
+        [ label [] [ text name ]
+        , paperSlider [ Attr.max "11" ] []
+        , label [] [ text (toString magnitude) ]
         ]
 
 
@@ -200,6 +214,11 @@ update msg model =
               }
             , Cmd.none
             )
+
+
+paperSlider : List (Attribute msg) -> List (Html msg) -> Html msg
+paperSlider =
+    node "paper-slider"
 
 
 initialCmd : Cmd Msg
