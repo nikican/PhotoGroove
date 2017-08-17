6d685ba91c29fd0bce13a26ba8fb894a2d3ac3e3
Nikica <nikica.novakovic@gmail.com>
2017-08-17 14:47:45 +0200

    Create fuzz test for JSON decoder

--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -4,14 +4,31 @@ import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
 import PhotoGroove exposing (..)
-import Json.Decode exposing (decodeString)
+import Json.Decode exposing (decodeValue)
+import Json.Encode as Encode
 
 
 decoderTest : Test
 decoderTest =
     test "title defaults to (untitled)" <|
         \_ ->
-            """{"url": "fruits.com", "size": 5}"""
-                |> decodeString photoDecoder
+            [ ( "url", Encode.string "fruits.com" )
+            , ( "size", Encode.int 5 )
+            ]
+                |> Encode.object
+                |> decodeValue photoDecoder
+                |> Result.map .title
+                |> Expect.equal (Ok "(untitled)")
+
+
+decoderFuzzTest : Test
+decoderFuzzTest =
+    fuzz2 string int "title defaults to (untitled) fuzz" <|
+        \url size ->
+            [ ( "url", Encode.string url )
+            , ( "size", Encode.int size )
+            ]
+                |> Encode.object
+                |> decodeValue photoDecoder
                 |> Result.map .title
                 |> Expect.equal (Ok "(untitled)")
