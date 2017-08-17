6d4f97cb74050bcf701bc6a86283ae63090cf199
Nikica <nikica.novakovic@gmail.com>
2017-08-17 16:13:51 +0200

    Create test for update function; Create test for fuzzing collection

--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -32,3 +32,31 @@ decoderFuzzTest =
                 |> decodeValue photoDecoder
                 |> Result.map .title
                 |> Expect.equal (Ok "(untitled)")
+
+
+selectByUrlSelectsPhoto : Test
+selectByUrlSelectsPhoto =
+    fuzz string " SelectByUrl selects the given photo by URL " <|
+        \url ->
+            PhotoGroove.initialModel
+                |> PhotoGroove.update (SelectByUrl url)
+                |> Tuple.first
+                |> .selectedUrl
+                |> Expect.equal (Just url)
+
+loadPhotosSelectsFirstPhoto : Test
+loadPhotosSelectsFirstPhoto =
+    fuzz (list string) "LoadPhotos selects the first photo" <|
+    \urls ->
+            let
+                photos =
+                List.map photoFromUrl urls
+            in
+                PhotoGroove.initialModel
+                |> PhotoGroove.update (LoadPhotos (Ok photos))
+                |> Tuple.first
+                |> .selectedUrl
+                |> Expect.equal (List.head urls)
+                photoFromUrl : String -> Photo
+                photoFromUrl url =
+                { url = url, size = 0, title = "" }
